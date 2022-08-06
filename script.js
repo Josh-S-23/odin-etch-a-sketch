24b8d4622cf7ebe024d9db4e8f0837ae3c869a2e
Josh-S-23 <joshrsantoss@hotmail.com>
2022-08-06 01:06:24 -0400

    Added function that allows user to select grid size

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 const container = document.querySelector('.container');
+const gridSize = document.querySelector('#gridSize');
 
-let rows = 16;
-let columns = 16;
+let rows;
+let columns;
+let msg;
 
-for (let i = 0; i < rows; i++) {
-    for (let x = 0; x < columns; x++) {
+for (let i = 0; i < 16; i++) {
+    for (let x = 0; x < 16; x++) {
         const grid = document.createElement('grid');
         grid.classList.add('grid');
-        grid.setAttribute('style', 'background: white; border: solid; border-width: 2.5px; transition-duration: 0.2s;')
+        container.setAttribute('style', 'display: grid; gap: 1px; margin-top: 65px; justify-content: center; grid-template-columns: repeat(16, 50px);');
+        grid.setAttribute('style', 'background: white; border: solid; border-width: 2.5px; transition-duration: 0.2s;');
 
         container.appendChild(grid);
 
         grid.addEventListener('mouseover', () => {
             grid.style.background = 'grey';
-        })
+        });
     }
 }
+
+function gridSizeBtn() {
+
+    let i = 1;
+
+    while (i != 0) {
+        let msg = prompt('please enter the number of squares per side for the new grid');
+
+        if (msg > 100) {
+            alert('Enter a number less than 100');
+            continue;
+        }
+
+        if (msg <= 100) {
+
+            container.textContent = '';
+
+            columns = msg;
+            rows = msg;
+
+
+            for (let i = 0; i < rows; i++) {
+                for (let x = 0; x < columns; x++) {
+                    const grid = document.createElement('grid');
+                    grid.classList.add('grid');
+                    container.setAttribute('style', 'display: grid; gap: 1px; margin-top: 65px; justify-content: center;');
+                    container.style.gridTemplateColumns = "repeat(" + msg + ", 50px)";
+                    grid.setAttribute('style', 'background: white; border: solid; border-width: 2.5px; transition-duration: 0.2s;');
+
+                    container.appendChild(grid);
+
+                    grid.addEventListener('mouseover', () => {
+                        grid.style.background = 'grey';
+                    });
+                }
+            }
+            break;
+        }
+    }
+}
+
+
+
+
+
+
 
 
 
